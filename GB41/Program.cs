80352969834010f069a1edbd4a5401314b579177
Garib <garibabbasov@yandex.ru>
2022-07-14 22:19:39 +0700

    add GBSeminar 6

---
+++ GB41/Program.cs
@@ -0,0 +1,47 @@
+﻿
+Console.Clear();
+
+string text = Input("Введите числа: ");
+int result = 0;
+
+for(int i = 0; i<text.Length; i++)
+{
+    if(text[i]=='0'||text[i]=='-')
+    {
+        for(int j = i+1; j<text.Length; j++)
+        {
+            if(text[j]==','||j+1==text.Length)
+            {
+                i = j;
+                break;
+            }        
+        }
+    }
+    else if(text[i]!=' ')
+    {
+        result++;
+        for(int j = i+1; j<text.Length; j++)
+        {
+            if(text[j]==','||j+1==text.Length)
+            {
+                i = j;
+                break;
+            }      
+        }
+    }
+}
+
+if (result>0)
+{
+    Console.WriteLine("Количество чисел больше 0: " + result);
+}
+else
+{
+    Console.WriteLine("В строке нет чисел больше 0");
+}
+
+string Input(string output)
+{
+    Console.WriteLine(output);
+    return Convert.ToString(Console.ReadLine());
+}
---
+++ GB43/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.Clear();
+
+double b1 = Input ("Введите значение b1: ");
+double k1 = Input ("Введите значение k1: ");
+double b2 = Input ("Введите значение b2: ");
+double k2 = Input ("Введите значение k2: ");
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+if(k1==k2)
+{
+    Console.WriteLine("Прямые параллельны, точек пересечения нет.\n");
+    return;
+}
+else
+{
+    Console.WriteLine($"y = {x}, x = {x} \n");
+}
+    
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
